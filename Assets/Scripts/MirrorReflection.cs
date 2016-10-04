e5bc6eba38d64c2372c7cfe65454f17842654033
BK-TN <me@bk-tn.com>
2016-10-04 21:21:05 +0200

    Placed some fences and dash panels + removed some old assets

--- Assets/Scripts/MirrorReflection.cs
+++ Assets/Scripts/MirrorReflection.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 // This is in fact just the Water script from Pro Standard Assets,
 // just with refraction stuff removed.
 
-[ExecuteInEditMode] // Make mirror live-update even when not in play mode
+//[ExecuteInEditMode] // Make mirror live-update even when not in play mode
 public class MirrorReflection : MonoBehaviour
 {
     public bool m_DisablePixelLights = true;
