50fa6d014852e720f48b7bdc787b14f898c0a200
BK-TN <me@bk-tn.com>
2016-10-22 23:09:46 +0200

    fuc 2

--- Subprojects/Core/GameVersion.cs
+++ Subprojects/Core/GameVersion.cs
@@ -3,13 +3,13 @@
     public class GameVersion
     {
         //The current version as a float, for checking which version is newest
-        public const float AS_FLOAT = 0.8f;
+        public const float AS_FLOAT = 0.81f;
 
         //To differentiate between testing builds and release builds
         public const bool IS_TESTING = false;
 
         //As a string, for displaying on the UI
-        public const string AS_STRING = "alpha v0.8";
+        public const string AS_STRING = "alpha v0.8.1";
 
         //Something stupid, usually unique for every version
         public const string TAGLINE = "Waaaaay past cool";
